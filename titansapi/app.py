48ab2718b28a44a36442979a2980b9db41ce202b
Mike Powell <mike@lakeslegendaries.com>
2022-03-13 12:05:41 -0400

    processing email subscriptions

--- titansapi/app.py
+++ titansapi/app.py
@@ -1,6 +1,9 @@
-from typing import Optional
+from os import remove
+from subprocess import run
 
 from fastapi import FastAPI
+
+from titansapi import __version__
 
 
 # create app
@@ -8,9 +11,16 @@ app = FastAPI()
 
 
 # root prompt
-@app.get("/")
+@app.get('/')
 def home():
     return {
-        "service": "titans-api",
-        "version": "0.0.0",
+        'service': 'titans-api',
+        'version': __version__,
     }
+
+
+@app.get('/subscribe/{email}')
+def subscribe(email: str):
+    print('', end='', file=open(email, 'w'))
+    run(f'az storage blob upload -c subscribe -f {email} -n {email}'.split())
+    remove(email)
