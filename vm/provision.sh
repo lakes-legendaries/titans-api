d357a8fc743e7c295d9d34e93404513ed31488a0
Mike Powell <mike@lakeslegendaries.com>
2022-03-13 10:37:00 -0400

    re-cloning on each reboot

--- vm/provision.sh
+++ vm/provision.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# error on failure
+set -e
+
 # setup unix
 sudo apt-get update
 sudo apt-get install -y ca-certificates curl git gnupg lsb-release
 
 # access docker repository
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg -y --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
@@ -14,12 +17,31 @@ echo \
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
-# build titans-api docker image
+# create startup command
+STARTUP=~/run-app.sh
+echo "
+#!/bin/bash
+
+# exit on error
+set -e
+
+# clone repo
+rm -rfd titans-api
 git clone https://github.com/lakes-legendaries/titans-api.git
+
+# build docker image
 cd titans-api
 sudo docker build -t titans-api .
+cd ..
 
-# start image on vm startup
-STARTUP=/etc/init.d/run-app.sh
-echo "sudo docker run -dp 80:80 titans-api" > $STARTUP
+# run docker container
+sudo docker run -dp 80:80 titans-api
+
+# clean up
+rm -rfd titans-api
+" > $STARTUP
 chmod +x $STARTUP
+
+# set startup command to run on reboot
+echo "@reboot ~/run-app.sh" | \
+    sudo tee /var/spool/cron/crontabs/root &> /dev/null
