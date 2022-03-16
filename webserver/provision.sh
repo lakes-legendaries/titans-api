6b80936b93bc0f05ccf97782aeba3114a81952f1
Mike Powell <mike@lakeslegendaries.com>
2022-03-16 09:30:00 -0400

    curling startup script

--- webserver/provision.sh
+++ webserver/provision.sh
@@ -28,53 +28,16 @@ sudo ln -s /snap/bin/certbot /usr/bin/certbot
 sudo certbot certonly --standalone -n --domains titansapi.eastus.cloudapp.azure.com
 
 # create startup command
-STARTUP=~/run-app.sh
-echo "
-#!/bin/bash
-
-# exit on error
-set -e
-
-# renew certificates
-sudo certbot renew
-
-# remove unused docker images and containers
-CONTAINERS=\$(sudo docker ps -aq)
-if [ ! -z \"\$CONTAINERS\" ]; then
-    sudo docker rm --force \$CONTAINERS
-fi
-sudo docker system prune --force --all
-
-# clone repo
-rm -rfd titans-api
-git clone https://github.com/lakes-legendaries/titans-api.git
-
-# copy certs into secrets
-for FILE in \
-    /etc/letsencrypt/live/titansapi.eastus.cloudapp.azure.com/fullchain.pem \
-    /etc/letsencrypt/live/titansapi.eastus.cloudapp.azure.com/privkey.pem \
-; do
-    sudo cp \$FILE ~/secrets/
-done
-
-# build docker image
-cd titans-api
-sudo docker build -t titans-api .
-cd ..
-
-# run docker container
-sudo docker run -dp 443:443 -v \$HOME/secrets:/secrets titans-api
-
-# clean up
-rm -rfd titans-api
-" > $STARTUP
-chmod +x $STARTUP
+REPO=https://raw.githubusercontent.com/lakes-legendaries/titans-api
+FILE=main/webserver/provision.sh
+curl $REPO/$FILE > ~/startup.sh
+chmod +x ~/startup.sh
 
 # set startup command to run on reboot, and set monthly reboot
 echo "
-@reboot $STARTUP
+@reboot ~/startup.sh
 0 0 1 * * reboot
 " | sudo tee /var/spool/cron/crontabs/root &> /dev/null
 
 # run startup script
-$STARTUP
+~/startup.sh
